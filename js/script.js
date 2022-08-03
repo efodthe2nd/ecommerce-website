db1f7fe008fb5865b0424265b8716c0f920da7be
David Efod <davidefod@gmail.com>
2022-08-03 14:41:31 +0100

    Add existing file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 0);
-});
+
+window.addEventListener("scroll", function(){
+	header.classList.toggle("sticky", window.scrollY > 0);
+} );
 
 let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let nav = document.querySelector('.nav-list');
 
-menu.onclick = () => {
+menu.onclick =() => {
 	menu.classList.toggle('bx-x');
-	navlist.classList.toggle('open');
+	nav.classList.toggle('open');
 };
 
 window.onscroll = () => {
 	menu.classList.remove('bx-x');
-	navlist.classList.remove('open');
+	nav.classList.remove('open');
 };
 
 const sr = ScrollReveal ({
-	distance: '30px',
+	distance:'30px',
 	duration: 2600,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:280, origin:'bottom'})
-
-sr.reveal('.featured,.cta,.new,.brand,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.home-text', {delay:280, origin:'bottom'});
+sr.reveal('.featured, .cta, .new, .brand, .contact', {delay:200, origin:'bottom'});
